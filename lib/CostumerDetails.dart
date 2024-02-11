b765372f2f65c71ab59ce3e473c3e7ed1f342c76
yosifBishara <70111190+yosifBishara@users.noreply.github.com>
2024-02-11 12:52:52 +0200

    update app version

--- lib/CostumerDetails.dart
+++ lib/CostumerDetails.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:kmel_bishara_app/myFirestore.dart';
--- lib/myFirestore.dart
+++ lib/myFirestore.dart
@@ -59,7 +59,6 @@ class FireHelper {
 
   Future<bool> deleteApp(String phone_num) async {
     try {
-      var s = await _firestore.collection('appointments').doc(phone_num);
       await _firestore.collection('appointments').doc(phone_num).delete();
       return true;
     } catch (e) {

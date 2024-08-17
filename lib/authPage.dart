f326fe88cf8b35c8443026f759f04586c2acad7a
yosifBishara <70111190+yosifBishara@users.noreply.github.com>
2024-08-18 00:58:30 +0300

    hide ios numeric keyboard

--- lib/authPage.dart
+++ lib/authPage.dart
@@ -14,6 +14,7 @@ class _AuthPageState extends State<AuthPage> {
 
   final _formKey = GlobalKey<FormState>();
   late String? nameField, phoneField;
+  FocusNode phoneNumberFocus = FocusNode();
 
   final controlName = TextEditingController(),controlNum = TextEditingController();
   bool isNameFieldVisible = false;
@@ -39,99 +40,61 @@ class _AuthPageState extends State<AuthPage> {
     ]);
 
     return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.grey[800],
-        body: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.98,
-            height: MediaQuery.of(context).size.height * 0.9,
-            child: Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.grey[800],
+          body: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.98,
+              height: MediaQuery.of(context).size.height * 0.9,
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
 
-                    //bottom loihab
-                    Center(
-                      child: Container(
-                        child: Image.asset('assets/app_icon.png'),
-                        width: MediaQuery.of(context).size.width * 0.6,
-                        height: MediaQuery.of(context).size.width * 0.5,
-                      ),
-                    ),
-
-                    // Phone num field
-                    TextFormField(
-                      enableSuggestions: true,
-                      // ignore: missing_return
-                      validator: (value) {
-                        if(controlNum.text.isEmpty)
-                          return 'הזן מספר טלפון';
-
-                        if (!(controlNum.text[0]=='0' && controlNum.text[1]=='5' && controlNum.text.length==10))
-                          return 'מספר טלפון לא תקין';
-                      },
-                      maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                      maxLength: 10,
-                      keyboardType: TextInputType.number,
-                      controller: controlNum,
-                      key: Key('phone number'),
-                      textAlign: TextAlign.end,
-                      decoration: InputDecoration(
-                        counterText: '',
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white60, width: 2.5),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black, width: 2.5),
-                        ),
-                        hintText: 'מספר טלפון',
-                        hintStyle: TextStyle(
-                          color: Colors.white,
+                      //bottom loihab
+                      Center(
+                        child: Container(
+                          child: Image.asset('assets/app_icon.png'),
+                          width: MediaQuery.of(context).size.width * 0.6,
+                          height: MediaQuery.of(context).size.width * 0.5,
                         ),
                       ),
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                      onChanged: (String newVal) async {
-                        if (newVal.length < 10){
-                          isKnownUserFlag = isNameFieldVisible = false;
-                          setState(() {});
-                          return;
-                        }
-                        isKnownUserFlag = await isKnownUser(newVal);
-                        isNameFieldVisible = !isKnownUserFlag;
-                        globalConfig.number = controlNum.text;
-                        setState(() {});
-                      },
-                    ),
 
-                    // Name field
-                    Visibility(
-                      visible: isNameFieldVisible,
-                      child: TextFormField(
+                      // Phone num field
+                      TextFormField(
+                        focusNode: phoneNumberFocus,
                         enableSuggestions: true,
                         // ignore: missing_return
-                        validator: (String? input) {
-                          if(input == null) return 'הזן שם';
-                          if(input.isEmpty) return 'הזן שם';
+                        validator: (value) {
+                          if(controlNum.text.isEmpty)
+                            return 'הזן מספר טלפון';
+
+                          if (!(controlNum.text[0]=='0' && controlNum.text[1]=='5' && controlNum.text.length==10))
+                            return 'מספר טלפון לא תקין';
                         },
-                        controller: controlName,
-                        key: Key('username'),
                         maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                        maxLength: 15,
+                        maxLength: 10,
+                        keyboardType: TextInputType.number,
+                        controller: controlNum,
+                        key: Key('phone number'),
                         textAlign: TextAlign.end,
                         decoration: InputDecoration(
+                          counterText: '',
                           focusedBorder: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.white60, width: 2.5),
                           ),
                           enabledBorder: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.black, width: 2.5),
                           ),
-                          hintText: 'שם מלא',
+                          hintText: 'מספר טלפון',
                           hintStyle: TextStyle(
                             color: Colors.white,
                           ),
@@ -139,87 +102,131 @@ class _AuthPageState extends State<AuthPage> {
                         style: TextStyle(
                           color: Colors.white,
                         ),
+                        onChanged: (String newVal) async {
+                          if (newVal.length < 10){
+                            isKnownUserFlag = isNameFieldVisible = false;
+                            setState(() {});
+                            return;
+                          }
+                          isKnownUserFlag = await isKnownUser(newVal);
+                          isNameFieldVisible = !isKnownUserFlag;
+                          globalConfig.number = controlNum.text;
+                          setState(() {});
+                        },
                       ),
-                    ),
 
-                    // Hello somebody!
-                    Visibility(
-                      visible: isKnownUserFlag,
-                      child: Center(
-                        child: Text(
-                          'שלום ' + globalConfig.name,
-                          textDirection: TextDirection.rtl,
-                          textAlign: TextAlign.center,
+                      // Name field
+                      Visibility(
+                        visible: isNameFieldVisible,
+                        child: TextFormField(
+                          enableSuggestions: true,
+                          // ignore: missing_return
+                          validator: (String? input) {
+                            if(input == null) return 'הזן שם';
+                            if(input.isEmpty) return 'הזן שם';
+                          },
+                          controller: controlName,
+                          key: Key('username'),
+                          maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                          maxLength: 15,
+                          textAlign: TextAlign.end,
+                          decoration: InputDecoration(
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white60, width: 2.5),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black, width: 2.5),
+                            ),
+                            hintText: 'שם מלא',
+                            hintStyle: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
                           style: TextStyle(
-                            fontSize: 20,
                             color: Colors.white,
                           ),
                         ),
                       ),
-                    ),
 
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.09,
-                      width: MediaQuery.of(context).size.width * 0.1
-                    ),
-
-                    // Enter System button
-                    Center(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black
-                        ),
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            globalConfig.name = controlName.text.isNotEmpty ? controlName.text : globalConfig.nextUserAppointment!.name ;
-                            Navigator.pushNamed(context, '/homePage');
-                          }
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.all(2.8,),
+                      // Hello somebody!
+                      Visibility(
+                        visible: isKnownUserFlag,
+                        child: Center(
                           child: Text(
-                            'כניסה',
+                            'שלום ' + globalConfig.name,
+                            textDirection: TextDirection.rtl,
+                            textAlign: TextAlign.center,
                             style: TextStyle(
-                              fontSize: 22,
+                              fontSize: 20,
                               color: Colors.white,
                             ),
                           ),
                         ),
                       ),
-                    ),
 
-                    //straight line
-                    Center(
-                      // padding: EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Container(
-                        height: 0.3,
-                        width: 360.0,
-                        color: Colors.white,
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * 0.09,
+                        width: MediaQuery.of(context).size.width * 0.1
                       ),
-                    ),
 
-                    // SizedBox(
-                    //     height: MediaQuery.of(context).size.height * 0.01,
-                    //     width: MediaQuery.of(context).size.width * 0.1
-                    // ),
-
-                    //CopyRigths~!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(Icons.copyright),
-                        SizedBox(width: 3,),
-                        Text(
-                          'Developed By: Yosif & Bishara - Bishara',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
+                      // Enter System button
+                      Center(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black
                           ),
-                        )
-                      ],
-                    )
-                  ],
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              globalConfig.name = controlName.text.isNotEmpty ? controlName.text : globalConfig.nextUserAppointment!.name ;
+                              Navigator.pushNamed(context, '/homePage');
+                            }
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.all(2.8,),
+                            child: Text(
+                              'כניסה',
+                              style: TextStyle(
+                                fontSize: 22,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      //straight line
+                      Center(
+                        // padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Container(
+                          height: 0.3,
+                          width: 360.0,
+                          color: Colors.white,
+                        ),
+                      ),
+
+                      // SizedBox(
+                      //     height: MediaQuery.of(context).size.height * 0.01,
+                      //     width: MediaQuery.of(context).size.width * 0.1
+                      // ),
+
+                      //CopyRigths~!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(Icons.copyright),
+                          SizedBox(width: 3,),
+                          Text(
+                            'Developed By: Yosif & Bishara - Bishara',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
